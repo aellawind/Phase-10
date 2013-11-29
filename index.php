ccd9731589ab6d9aada59405912be49363c3abbc
Amira Anuar <amiraanuar@g.harvard.edu>
2013-11-29 15:27:27 -0500

    Starter CSS

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
 <!DOCTYPE html>
 <html>
+<head>
 
-testtt
+	<title>Amira's Phase 10</title>
+	<link rel="shortcut icon" href="images/phase10.ico"/>
+	
+	<link rel="stylesheet" href="css/cards.css" type="text/css">
+	<link rel="stylesheet" href="css/game.css" type="text/css">
 
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+
+</head>
+
+<body>
+
+
+<script src="js/phase10logic.js"></script>
+<script src="js/phase10cards.js"></script>
+</body>
 </html>
